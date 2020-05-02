927e1b262e5a14a4019dce05a09de847d99bd08f
NiVanc <nico@izebra.be>
2020-05-02 14:21:16 +0200

    Create the necessary components

---
+++ src/components/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>The header component</h1>
+  </div>
+</template>
---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>The portfolio: stock component</h1>
+  </div>
+</template>
