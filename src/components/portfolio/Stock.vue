9b35f456c1d94d33329a63e77fda6ceb682d591c
NiVanc <nico@izebra.be>
2020-05-06 23:49:53 +0200

    List stocks on portfolio page

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
-    <h1>The portfolio: stock component</h1>
+    <div class="col-sm-6 col-md-4">
+      <div class="panel panel-success">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            {{ stock.name }}
+            <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
+          </h3>
+        </div>
+        <div class="panel-body">
+          <div class="pull-left">
+            <input type="number" class="form-control" placeholder="Quantity" v-model="quantity" />
+          </div>
+          <div class="pull-right">
+            <button
+              @click="sellStock"
+              class="btn btn-success"
+              :disabled="quantity <= 0 || !Number.isInteger(+quantity)"
+            >Sell</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  props: ["stock"],
+  methods: {
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      this.$store.dispatch("sellStock", order);
+    }
+  }
+};
+</script>
